36486728834f70a848ecb48d915ac8441b535255
Michael Weilbächer <michael.weilbaecher@senacor.com>
2022-12-12 12:22:23 +0100

    line color

--- src/js/app.js
+++ src/js/app.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         "type": "polygon",
         "stroke": {
           "width": 0,
-          "color": "#000000"
+          "color": "#29B7B3"
         },
         "polygon": {
           "nb_sides": 5
